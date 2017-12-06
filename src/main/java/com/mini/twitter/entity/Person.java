1bf8ef7996a2496ffc943ce75d8e1a44d99d3a2e
Daniel AHMED <daniel.ahmedinfo@gmail.com>
2017-12-06 14:43:51 +0100

    V2

---
+++ src/main/java/com/mini/twitter/entity/Person.java
@@ -0,0 +1,23 @@
+package com.mini.twitter.entity;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.api.server.spi.config.Api;
+
+@PersistenceCapable
+public class Person {
+	
+	@PrimaryKey
+	@Persistent(valueStrategy=IdGeneratorStrategy.IDENTITY)
+	private String id;
+
+	@Persistent
+	private String givenName;
+	
+	@Persistent
+	private String familyName;
+
+}
