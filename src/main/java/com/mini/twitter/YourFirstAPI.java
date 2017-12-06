efee909d844cd440f90144247263beb3a23ab6a2
Daniel AHMED <daniel.ahmedinfo@gmail.com>
2017-12-06 13:48:54 +0100

    V2

--- src/main/java/com/mini/twitter/YourFirstAPI.java
+++ src/main/java/com/mini/twitter/YourFirstAPI.java
@@ -6,7 +6,7 @@ import com.google.api.server.spi.config.Api;
   * Add your first API methods in this class, or you may create another class. In that case, please
   * update your web.xml accordingly.
  **/
-@Api(name = "skeleton-api",
+@Api(name = "skeleton",
      version = "v1")
 public class YourFirstAPI {
 }
